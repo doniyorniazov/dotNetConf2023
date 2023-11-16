504298a429ebb7ca48167e874f00c4341999c525
doniyorniazov <niazovd@gmail.com>
2023-11-16 19:16:53 +0500

    create project

---
+++ api/Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("/")]
+public class MainController : ControllerBase
+{
+
+    [HttpGet(Name = "/")]
+    public string Get()
+    {
+        string? message = "Hi Devs! Welcome to .NET Conf 2023 Khujand";
+        message = string.IsNullOrEmpty(message) ? "No version: No environment variable specified. Please specify MESSAGE environment variable." : message;
+
+        return $"V1: {message}";
+    }
+}
