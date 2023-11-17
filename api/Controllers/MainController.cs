16d050cb735ef1b34807baa12605c88fbd84e3b9
doniyorniazov <niazovd@gmail.com>
2023-11-17 17:30:54 +0500

    update main controller

--- api/Controllers/MainController.cs
+++ api/Controllers/MainController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
 namespace api.Controllers;
 
 [ApiController]
@@ -11,8 +10,6 @@ public class MainController : ControllerBase
     public string Get()
     {
         string? message = "Hi Devs! Welcome to .NET Conf 2023 Khujand.";
-        message = string.IsNullOrEmpty(message) ? "No version: No environment variable specified. Please specify MESSAGE environment variable." : message;
-
-        return $"V2: {message}";
+        return $"V1: {message}";
     }
 }
