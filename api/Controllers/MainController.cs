4f2ab3c122be93103cdaa5f3dff0d8325c075df8
doniyorniazov <niazovd@gmail.com>
2023-11-17 21:24:38 +0500

    update main controller

--- api/Controllers/MainController.cs
+++ api/Controllers/MainController.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 namespace api.Controllers;
-
 [ApiController]
 [Route("/")]
 public class MainController : ControllerBase
 {
-
     [HttpGet(Name = "/")]
     public string Get()
     {
-        string? message = "Hi Devs! Welcome to .NET Conf 2023 Khujand.";
+        string message = "Hi Devs! Welcome to .NET Conf 2023 Khujand.";
         return $"V1: {message}";
     }
 }
